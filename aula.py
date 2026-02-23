224692880fa72d66e03bec333fd1e04c7efb51fc
Manu <emanuelle.santos02@escola.pr.gov.br>
2026-02-23 09:45:53 -0300

    Add files via upload

---
+++ aula.py
@@ -0,0 +1,14 @@
+nome = "Emanuelle"
+idade = 16
+altura = 1.69
+genero = "bisexual"
+cor_de_pele = "branca"
+cor_de_cabelo = "ruivo"
+tamanho_cabelo = "medio"
+estilo_cabelo = "ondulado"
+curso = "Tecnico em Desenvolvimentos de Sistemas"
+hobby = "escutar musica"
+
+# 4 - Utilizando a função print(), mostre o texto completo da sua autodescrição, utilizando as variáveis que você criou!
+# Exemplo: "Olá, meu nome é Fulano, tenho 999 anos e 1.90 de altura... (continue até usar todas as variáveis)"
+print(f"Oii, meu nome é {nome}, tenho {idade} anos, tenho {altura} de altura, sou {genero}, sou {cor_de_pele}, meu cabelo é {cor_de_cabelo} o tamanho dele é {tamanho_cabelo}, ele é {estilo_cabelo}, faço curso em {curso}, gosto de {hobby}")
